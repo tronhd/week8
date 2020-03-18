f4bdc0937d3e19e043e4286060e4923201f5c581
tronhd <taronhead@gmail.com>
2020-03-18 19:41:43 -0400

    update

--- mysql.js
+++ mysql.js
@@ -1,19 +1,3 @@
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'example.org',
-//   user     : 'bob',
-//   password : 'secret'
-// });
- 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });
-
 // Exercise 1
 // Fast Food app (Frontend)
 // Create an html text input box, our customers are going to put their orders in here. Their orders are going to look like: " french fries: 1, shakes: 3"
@@ -24,7 +8,7 @@
 // Bonus Make a string with the customer's order in it and repeat back to the customer what they ordered.
 
 var menu=[
-  ["wafffle fries", 50],
+  ["waffle fries", 50],
   ["blizzard", 70],
   ["cheessy gordita crunch", 70],
   ["cheese burger", 60],
@@ -36,31 +20,41 @@ var menu=[
   ["ultimate fondue", 13]
 ];
 
-var order = document.getElementById("order");
-var input = document.getElementById("userInput");
+//var order = document.getElementById("order");
 
-function myPrimeFunction(){
-myPrime = setInterval(function(){ displayPrime("first parameter");}, 3000);
+// function myPrimeFunction(){
+//  myPrime = setInterval(function(){ displayPrime("first parameter");}, 3000);
+// }
+
+// function order1(){
+//    if(document.getElementById("userInput")==menu[i][i]){
+//      document.getElementById("demo").innerHTML;
+//    } else{
+//      console.log("error");
+//    }
+//   };
+
+function order(){
+var input = document.getElementById('userInput').value;
+var valid = false;
+for(var i=0; i<menu.length; i++) {
+  if(input==menu[i][0]){
+    console.log(menu[i][0]);
+    valid = true;
+  } 
+} 
+if(!valid) {
+  console.log('try again!');
+}
 }
 
-function order1(){
-  if(input==menu[i][i]){
-    document.getElementById("demo").innerHTML;
-  } else{
-    console.log("error");
-  }
- };
-
-//  function seeStock(){
-//   var input = document.getElementById('userInput').value;
-//   var valid = false;
-//   for(var i=0; i<fruits.length; i++) {
-//     if(input==fruits[i][0]){
-//       console.log(fruits[i][1]);
-//       valid = true;
-//     } 
-//   } 
-//   if(!valid) {
-//     console.log('try again!');
-//   }
-//  }
+// Exercise 2
+// Fast Food app (Frontend)
+// Create an object named menu where we'll save the menu for the restaurant. For each property (Each property should be a string like "fries") and add a number as the value. That will be the amount of stock the store has.
+// Take the customerOrder variable, (which should be a string) and split it on the comma character. Save the result to the order array variable. This will turn it into an array that is comma delimeted. Because usually there is a space after a comma we are going to need to trim the white space. This will make it so there is no extra white space.
+// Create a loop (can be any kind of loop, while, for, foreach whatever you are comfortable with). Loop through the entire order array (from 0 to the length of the array)
+// In the loop split each item of this array on the colon character. You will probably have to trim the leading and trailing whitespace again.
+// Now that we have the customer's item they would like to order, create a conditional (if) statement and check to see if the customer actually ordered something off of the menu. This is going to require a for...in loop to loop through the menu object. Just check to see if the property exists. If it doesn't exist log to the console how the customer did not order something off of the menu.
+// If the previous statement was true make another conditional statement that checks to see if there is enough stock in the restaurant to fulfill the customer's order. If there is not enough, then let the customer know that you can not fulfill that particular order.
+// If the customer ordered something on the menu and it is in stock then deduct the number the customer ordered from the restaurant's stock.
+// Bonus Create a button that you could click to display the restaurant's inventory.
